f2a6fc9f82bce3139c3da64b3ec972135ec0be41
azappon <61697504+azappon@users.noreply.github.com>
2022-05-26 00:35:14 +0200

    Update 242 Valid Anagram.py

--- 242 Valid Anagram.py
+++ 242 Valid Anagram.py
@@ -1,4 +1,5 @@
 class Solution: # elegant pythonic solution
     def isAnagram(self, s: str, t: str) -> bool:
+        # cases like "aa"/"a" would not work using se(s)!
         if Counter(s) == Counter(t):
             return True
